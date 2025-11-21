5d1d527d64bcf4d91a89579b19ae402e04540135
Person-0 <atulyajain28122007@gmail.com>
2025-11-21 22:44:55 +0530

    made the font family universal on the entire webpage

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
 import "../html/index.css";
+
+const _pageElementsIds = {
+    "login": "submit-login-btn",
+    "signup": "signup-login-btn",
+    "accToggleBtn": "acc-toggle-btn",
+    "usernameInput": "username-input",
+    "passwordInput": "password-input"
+}
+type PageElementName = keyof typeof _pageElementsIds;
+
+const pageElements = {} as Record<PageElementName, HTMLElement>;
+
+for(const [name, id] of Object.entries(_pageElementsIds)) {
+    const el = document.getElementById(id);
+    if(el) {
+        pageElements[name as PageElementName] = el;
+    } else {
+        pageElements[name as PageElementName] = document.createElement("button");
+        console.error("Button [" + name + "] with ID [" + id + "] was not found.");
+    }
+}
