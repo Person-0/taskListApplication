60073a0b16cf513ea5bac2dabd02de41c5c895e6
Person-0 <atulyajain28122007@gmail.com>
2025-11-21 22:58:23 +0530

    added a tab based system to switch between "pages" without the need of loading another webpage

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,35 @@ const _pageElementsIds = {
     "usernameInput": "username-input",
     "passwordInput": "password-input"
 }
+
+type tabName = "login" | "app";
 type PageElementName = keyof typeof _pageElementsIds;
 
 const pageElements = {} as Record<PageElementName, HTMLElement>;
-
-for(const [name, id] of Object.entries(_pageElementsIds)) {
+for (const [name, id] of Object.entries(_pageElementsIds)) {
     const el = document.getElementById(id);
-    if(el) {
+    if (el) {
         pageElements[name as PageElementName] = el;
     } else {
         pageElements[name as PageElementName] = document.createElement("button");
         console.error("Button [" + name + "] with ID [" + id + "] was not found.");
     }
 }
+
+function switchTab(tabName: tabName) {
+    const toShowTab = document.querySelector("[tab-name=" + tabName + "]") as HTMLElement;
+    if (toShowTab) {
+        for (const tab of (document.querySelectorAll(".page-tab") as NodeListOf<HTMLElement>)) {
+            tab.style.display = "none";
+        }
+        toShowTab.style.display = "flex";
+    } else {
+        console.error("switchTab called with invalid tabName: ", tabName);
+    }
+}
+
+pageElements.login.onclick = () => {
+    switchTab("app");
+}
+
+switchTab("login");
